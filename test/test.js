bed4f1c98299adf9e02a35abcc3f3da80ef895ec
srjsnehanshu <57353992+srjsnehanshu@users.noreply.github.com>
2020-12-23 02:45:57 +0530

    heroku try 9

--- test/test.js
+++ test/test.js
@@ -45,10 +45,10 @@ describe('Tasks API', () => {
         it("It should POST a new task", function(done) {
             this.timeout(10000)
             const task = {
-                username: "user14",
+                username: "user15",
                 fullname: "full name",
                 password: 1234568,
-                email: "user14email@gmail.com"
+                email: "user15email@gmail.com"
             };
             chai.request(server)                
                 .post("/register")
